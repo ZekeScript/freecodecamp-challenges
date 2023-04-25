f1d27af72744e3cebc29a1993b2b06cfab3451a2
ezEst <estiga27@gmail.com>
2023-04-24 23:41:20 -0300

    Add palindrome checker
    This commit adds a new function to the code that checks whether a given string is a palindrome or not. A palindrome is a word, phrase, number, or other sequence of characters that reads the same forward and backward.

---
+++ Palindrome_checker.js
@@ -0,0 +1,25 @@
+function palindrome(str) {
+	let str1 = str.toLowerCase().replace(/[^a-z0-9]/g, '');
+	for (let i = 0, len = str1.length - 1; i <= str1.length / 2; i++) {
+		if (str1[i] !== str1[len - i]) {
+			return false;
+		}
+	}
+	return true;
+}
+
+console.log('eye: ', palindrome('eye'));
+console.log('_eye: ', palindrome('_eye'));
+console.log('race car: ', palindrome('race car'));
+console.log('not a palindrome: ', palindrome('not a palindrome'));
+console.log(
+	'A man, a plan, a canal. Panama: ',
+	palindrome('A man, a plan, a canal. Panama')
+);
+console.log('never odd or even: ', palindrome('never odd or even'));
+console.log('nope: ', palindrome('nope'));
+console.log('almostomla: ', palindrome('almostomla'));
+console.log('My age is 0, 0 si ega ym.: ', palindrome('My age is 0, 0 si ega ym.'));
+console.log('1 eye for of 1 eye.: ', palindrome('1 eye for of 1 eye.'));
+console.log('0_0 (: /- :) 0-0: ', palindrome('0_0 (: /- :) 0-0'));
+console.log('five|_/|four: ', palindrome('five|_/|four'));
